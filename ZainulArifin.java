106ca8c3f347a83a22aaf155eb5788398cfdc564
ZainulArifinKipli <43515980+ZainulArifinKipli@users.noreply.github.com>
2018-09-23 19:20:08 +0700

    Add files via upload

---
+++ ZainulArifin.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zainularifin;
+
+/**
+ *
+ * @author Diva
+ */
+public class ZainulArifin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+             int umur=23;
+        String nama="Zainul Arifin";
+           
+
+          boolean cantik = false;
+         if(umur>23 && cantik==true){
+           System.out.println("Sangat mau");
+       }
+         else if(umur>23 ){
+           System.out.println("Saya pikir dulu");
+       }
+       else {
+           System.out.println("maaf saya tidak mau");
+       }
+        
+    
+    }
+    
+}
